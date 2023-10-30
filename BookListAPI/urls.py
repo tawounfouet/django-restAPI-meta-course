b2d92706a02ba3b7c38cd5760210805f48cc68f3
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-10-30 22:43:38 +0100

    adding class based view to our api

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books),
+    #path('books/', views.books),
+     path('books/', views.BookList.as_view()),
+     path('books/<int:pk>/', views.Book.as_view()),
  
 ]
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -11,6 +11,8 @@ from .models import Book
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
 
+
+# Using csrf_exempt decorator
 # @csrf_exempt
 # def books(request):
 #     if request.method == 'GET':
@@ -34,11 +36,17 @@ from django.forms.models import model_to_dict
 
 
 
+# Without using api_view decorator
+# def books(request):
+#     return HttpResponse('List of the books', status=status.HTTP_200_OK)
+
+
+# Using api_view decorator 
 @api_view(['GET', 'POST'])
 def books(request):
     return Response('List of the books', status=status.HTTP_200_OK)
 
-
+# Using api_view decorator and context parameter
 # @api_view()
 # def books(request):
 #     books = [
@@ -46,3 +54,32 @@ def books(request):
 #         {'id': 2, 'title': 'Book 2'},
 #     ]
 #     return Response({'List of the books': books},  status=status.HTTP_200_OK)
+
+
+
+# Using class based views
+from rest_framework.views import APIView
+
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if (author):
+            return Response({"message":"list of the books by " + author}, status=status.HTTP_200_OK)
+        
+        return Response({"message":"List of the books"}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        #title = request.data.get('title')
+        #return Response({"message":"new book created"}, status=status.HTTP_201_CREATED)
+
+        # Using payload : a payload is a body of data sent through POST method
+        return Response({"title":request.data.get('title')}, status=status.HTTP_201_CREATED)
+        #return Response({"title":title}, status=status.HTTP_201_CREATED)
+
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({"message":"Single book with id " + str(pk)}, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')}, status=status.HTTP_200_OK)
